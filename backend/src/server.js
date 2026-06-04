03b597bfda74cf51efe9b48d655531e8a57496a6
TXCuon9 <xcuongai512@gmail.com>
2026-06-04 13:45:32 +0700

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,7 @@ app.get("/books", (req, res) => {
 // make ready for deploy
 if(ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
-    app.get("*", (req, res) => {
+    app.get("/*splat", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     })
 }
